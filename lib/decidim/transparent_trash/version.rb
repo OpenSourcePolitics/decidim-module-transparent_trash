95c48066017057037f23c74e033013d6ad0979c7
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-03-06 10:48:52 +0100

    Update GH workflow (#4)
    * Update GH workflow
    
    * Define self.version and use Decidim rubygems
    
    * Update rubocop rules
    
    * Update versions in specs
    
    * Remove not needed comment
    
    * Add initiative fix count from #10150

--- lib/decidim/transparent_trash/version.rb
+++ lib/decidim/transparent_trash/version.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 module Decidim
-  # This holds the decidim-meetings version.
   module TransparentTrash
     def self.version
+      "0.0.1"
+    end
+
+    def self.decidim_version
       "0.27.1"
     end
   end
